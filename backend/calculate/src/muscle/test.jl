d2a439a11a6bcd56a7864f5767eea9714c827436
Eros <eros.montin@gmail.com>
2026-05-04 18:56:59 -0400

    Remove old v1 backend files; keep data, integrations, readme, LICENSE

--- backend/calculate/src/muscle/test.jl
+++
@@ -1,7 +0,0 @@
-using KomaNYU
-using NIfTI
-using NPZ
-using JSON
-using Printf
-using OpenSpecFun_jll
-println("Testing OpenSpecFun artifact: ", ispath(OpenSpecFun_jll.artifact_dir) ? "Found" : "Missing")
