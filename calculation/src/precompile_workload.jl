ec69c62d5866e3bf9eb382e80d44235e56a48f06
Eros <eros.montin@gmail.com>
2026-06-25 20:45:40 -0400

    camrie-tools v2

--- calculation/src/precompile_workload.jl
+++ calculation/src/precompile_workload.jl
@@ -1,18 +1,4 @@
-using KomaMRI
 using KomaInterface
-using NPZ
-using JSON
-using HDF5
-using LinearAlgebra
 
 println("CAMRIE CPU precompile workload starting")
-
-sys = Scanner()
-obj = brain_phantom2D()
-seq = PulseDesigner.EPI_example()
-sim_params = KomaMRICore.default_sim_params()
-sim_params["gpu"] = false
-sim_params["Nthreads"] = 1
-
-sig = simulate(obj[1:1], seq, sys; sim_params)
-println("CPU precompile signal type: ", typeof(sig))
+println("KomaInterface loaded: ", KomaInterface)
